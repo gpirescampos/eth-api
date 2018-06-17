6815fa3854fc0230848435571840f0417419983d
Guilherme Campos <guilhermecampos@Guilhermes-MacBook-Pro.local>
2018-06-17 23:25:53 +0100

    Refactored code: using interface instead of assembly call now

--- contracts/HelloWorldController.sol
+++ contracts/HelloWorldController.sol
@@ -5,26 +5,21 @@ contract HelloWorldController {
   mapping (string => address) method;
 
   constructor() public {
-    owner = msg.sender;
   }
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function addMethod(string _methodName, address _methodAddress) public restricted {
+  function addMethod(string _methodName, address _methodAddress) public {
     method[_methodName] = _methodAddress;
   }
 
-  function removeMethod(string _methodName) public restricted {
+  function removeMethod(string _methodName) public {
     method[_methodName] = address(0);
   }
 
-  function checkMethod(string _methodName) public view restricted returns(bool) {
-    return method[_methodName] == address(0);
+  function checkMethod(string _methodName) public view returns(address) {
+    return method[_methodName];
   }
 
-  function callMethod(string _method) public view returns (string) {
+  function callMethod(string _method, string _params) public view returns (string) {
     return "a";
   }
 
--- test/TestRouter.js
+++ test/TestRouter.js
@@ -2,27 +2,23 @@ var Router = artifacts.require("Router");
 var HelloWorldController = artifacts.require("HelloWorldController");
 
 contract('RouterInit', async () => {
-  it("add HelloWorld as a new Controller", async () => {
-    let instance = await Router.deployed();
-    await instance.addController("HelloWorld", "0x0");
-    let res = await instance.checkController("HelloWorld");
-    assert.equal(true, res);
-  })
-})
-
-contract('HelloWorldControllerInit', async () => {
-  it("add Get method to HelloWorld Controller", async () => {
-    let instance = await HelloWorldController.deployed();
-    await instance.addMethod("Get", "0x0");
-    let res = await instance.checkMethod("Get");
-    assert.equal(true, res);
+  it("Init everything", async () => {
+    let instanceC = await HelloWorldController.deployed();
+    await instanceC.addMethod("Get", instanceC.address);
+    let resC = await instanceC.checkMethod("Get");
+    console.log(resC);
+    assert.notEqual("0x0000000000000000000000000000000000000000", resC);
+    let instanceR = await Router.deployed();
+    await instanceR.addController("HelloWorld", instanceC.address);
+    let resR = await instanceR.checkController("HelloWorld");
+    assert.notEqual("0x0000000000000000000000000000000000000000", resR);
   })
 })
 
 contract("RouterTest", async () => {
   it("call Get method from HelloWorld controller", async () => {
     let instance = await Router.deployed();
-    let res = await instance.callController("HelloWorld", "Get", [], {gas: 500000});
+    let res = await instance.callController("HelloWorld", "Get", "");
     console.log(res);
   })
 })
