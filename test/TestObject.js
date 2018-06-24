c820daab506fa3fccae83119accaa0284e0d55dd
Guilherme Campos <guilherme.campos@consenys.net>
2018-06-24 15:48:00 +0100

    Added test for Object

--- test/TestObject.js
+++ test/TestObject.js
@@ -1,8 +1,32 @@
 var Obj = artifacts.require("Object");
 
+function iThrowError(instance) {
+  try {
+    instance.object()
+  } catch (error) {
+    throw new Error("Error thrown"); 
+  }
+}
+
 contract('Object init', async () => {
-  it("Inits Object", async () => {
-    const o = await Obj.new("HelloWorld")
-    assert.equal(o.address.length, 42)
+  it("POST", async () => {
+    const contractO = await Obj.new("HelloWorld")
+    assert.equal(contractO.address.length, 42)
+  })
+  it("GET", async () => {
+    const contractO = await Obj.new("HelloWorld")
+    const obj = await contractO.object()
+    assert.equal(obj, "HelloWorld")
+  })
+  it("PUT", async () => {
+    const contractO = await Obj.new("HelloWorld")
+    await contractO.PUT("Hello")
+    const obj = await contractO.object()
+    assert.equal(obj, "Hello")
+  })
+  it("DELETE", async () => {
+    const contractO = await Obj.new("HelloWorld")
+    await contractO.DELETE()
+    assert.throws(iThrowError, Error)
   })
 })
